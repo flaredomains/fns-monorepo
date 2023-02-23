9961ba0b2edf107e2cca9ce5ab9a735dd162662c
Chase Brown <Chasebrown7@icloud.com>
2023-02-23 14:58:08 -0700

    Minor revisions and additions to the forge environment so code builds successfully

---
+++ apps/forge/src/users/Actor.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.6;
+pragma experimental ABIEncoderV2;
+
+contract Actor {
+
+    /// @notice For receiving Ether.
+    /// @dev No further implementation needed.
+    receive() external payable {}
+
+    /*********************/
+    /*** TRY FUNCTIONS ***/
+    /*********************/
+
+    // function try_transferToken(address token, address to, uint256 amount) external returns (bool ok) {
+    //     string memory sig = "transfer(address,uint256)";
+    //     (ok,) = address(token).call(abi.encodeWithSignature(sig, to, amount));
+    // }
+
+}
--- apps/forge/test/ENSRegistry.t.sol
+++ apps/forge/test/ENSRegistry.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.6;
 pragma abicoder v2;
 
-import "forge-std/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 import "../src/ENSRegistry.sol";
 
 contract TestENSRegistry is Test {
