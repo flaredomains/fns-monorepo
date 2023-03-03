3c79b268bdb7d99f1298c0ba27242b06356eadc9
Ric Waltman <rickyontheblock@pm.me>
2023-03-02 23:46:02 -0700

    ReverseRegistrar claim & claimForAddr Tests

--- apps/forge/test/registry/ReverseRegistrar.t.sol
+++ apps/forge/test/registry/ReverseRegistrar.t.sol
@@ -17,6 +17,7 @@ import "fns-test/utils/HardhatAddresses.sol";
 contract TestReverseRegistrar is Test {
     ENSRegistry public ensRegistry;
     ReverseRegistrar public reverseRegistrar;
+    ReverseRegistrar public dummyOwnableContract;
     PublicResolver public publicResolver;
     DummyNameWrapper public nameWrapper;
 
@@ -30,12 +31,16 @@ contract TestReverseRegistrar is Test {
     bytes32 addr0ReverseNode = ENSNamehash.getReverseNode(address0);
     bytes32 addr1ReverseNode = ENSNamehash.getReverseNode(address1);
     bytes32 addr2ReverseNode = ENSNamehash.getReverseNode(address2);
+    bytes32 dummyOwnableReverseNode;
 
     function setUp() public {
-
         ensRegistry = new ENSRegistry();
         nameWrapper = new DummyNameWrapper();
         reverseRegistrar = new ReverseRegistrar(ensRegistry);
+
+        // Setup another ReverseRegistrar as a dummy ownable contract, because it is ownable
+        dummyOwnableContract = new ReverseRegistrar(ensRegistry);
+        dummyOwnableReverseNode = ENSNamehash.getReverseNode(address(dummyOwnableContract));
 
         // A mocked nameWrapper is used here to always return the caller's address for 'ownerOf'
         publicResolver = new PublicResolver(
@@ -62,6 +67,7 @@ contract TestReverseRegistrar is Test {
         console.logBytes32(addr2ReverseNode);
     }
 
+    // Original ENS test: 'should calculate node hash correctly'
     function testShouldCalcNodeHashCorrectly() public {
         assertEq(reverseRegistrar.node(address(this)), thisReverseNode);
         assertEq(reverseRegistrar.node(address0), addr0ReverseNode);
@@ -69,9 +75,77 @@ contract TestReverseRegistrar is Test {
         assertEq(reverseRegistrar.node(address2), addr2ReverseNode);
     }
 
+    // Original ENS test: 'claim'::'allows an account to claim its address'
     function testClaimAllowsAccountToClaimItsAddress() public {
         bytes32 nodehash = reverseRegistrar.claim(address1);
         assertEq(nodehash, thisReverseNode);
         assertEq(ensRegistry.owner(thisReverseNode), address1);
     }
+
+    // Original ENS test: 'claim'::'event ReverseClaimed is emitted'
+    event ReverseClaimed(address indexed addr, bytes32 indexed node);
+    function testClaimEventReverseClaimedIsEmitted() public {
+        vm.expectEmit(true, true, false, true, address(reverseRegistrar));
+
+        // We emit the event that we expect to see
+        // NOTE: The event emitted will be the reverse being claimed for the caller, 
+        //       not the new owner
+        emit ReverseClaimed(address(this), thisReverseNode);
+
+        // Then we perform the call
+        reverseRegistrar.claim(address1);
+    }
+
+    // Original ENS test: 'claimForAddr'::'allows an account to claim its address'
+    function testClaimForAddrAllowsAccountToClaimItsAddress() public {
+        bytes32 nodehash = reverseRegistrar.claimForAddr(address(this), address1, address(publicResolver));
+        assertEq(nodehash, thisReverseNode);
+        assertEq(ensRegistry.owner(thisReverseNode), address1);
+    }
+
+    // Original ENS test: 'claimForAddr'::'event ReverseClaimed is emitted'
+    function testClaimForAddrEventReverseClaimedIsEmitted() public {
+        vm.expectEmit(true, true, false, true, address(reverseRegistrar));
+
+        // We emit the event that we expect to see
+        // NOTE: The event emitted will be the reverse being claimed for the caller, 
+        //       not the new owner
+        emit ReverseClaimed(address(this), thisReverseNode);
+
+        // Then we perform the call
+        reverseRegistrar.claimForAddr(address(this), address1, address(publicResolver)); 
+    }
+
+    // Original ENS test: 'claimForAddr'::'forbids an account to claim another address'
+    function testClaimForAddrForbidsAccountClaimingAnotherAddress() public {
+        vm.expectRevert();
+        reverseRegistrar.claimForAddr(address1, address(this), address(publicResolver));
+    }
+
+    // Original ENS test: 'claimForAddr'::'allows an authorised account to claim a different address'
+    function testClaimForAddrAllowsAuthorisedAccountToClaimDifferentAddress() public {
+        // First, grant approval on this test contract from address1 as sender
+        vm.prank(address1);
+        ensRegistry.setApprovalForAll(address(this), true);
+
+        reverseRegistrar.claimForAddr(address1, address2, address(publicResolver));
+        assertEq(ensRegistry.owner(addr1ReverseNode), address2);
+    }
+    
+    // Original ENS test: 'claimForAddr'::'allows a controller to claim a different address'
+    function testClaimForAddrAllowsControllerClaimDifferentAddress() public {
+        reverseRegistrar.setController(address(this), true);
+        reverseRegistrar.claimForAddr(address1, address2, address(publicResolver));
+        assertEq(ensRegistry.owner(addr1ReverseNode), address2);
+    }
+
+    // Original ENS test: 'claimForAddr'::'allows an owner() of a contract to claim the reverse node of that contract'
+    function testClaimForAddrAllowsContractOwnerToClaimReverseNodeOfContract() public {
+        reverseRegistrar.setController(address(this), true);
+        reverseRegistrar.claimForAddr(address(dummyOwnableContract), address(this), address(publicResolver));
+        assertEq(ensRegistry.owner(dummyOwnableReverseNode), address(this));
+    }
+
+
+
 }
