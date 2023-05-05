e0bb44477951e5fe4e41e09df79ad7f362397299
Chase Brown <Chasebrown7@icloud.com>
2023-05-05 16:26:37 -0700

    New commit animation

--- apps/next/components/Register/Bottom.tsx
+++ apps/next/components/Register/Bottom.tsx
@@ -45,6 +45,34 @@ const Waiting = () => {
   )
 }
 
+const Committing = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center px-6 py-3 bg-[#F97316] h-12 rounded-lg">
+        <svg
+          aria-hidden="true"
+          className={`mr-2 w-4 h-4 text-[#ffffff] dark:text-gray-500 animate-spin fill-[#ffffff]`}
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentFill"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="#F97316"
+          />
+        </svg>
+        <p className="pl-2 text-white text-base font-semibold text-center">
+          Committing
+        </p>
+      </div>
+    </>
+  )
+}
+
 const ReqToRegister = ({
   result,
   regPeriod,
@@ -60,6 +88,7 @@ const ReqToRegister = ({
 }) => {
   const [registerReady, setRegisterReady] = useState(false)
   const [isWaiting, setIsWaiting] = useState(false)
+  const [isCommitting, setIsCommitting] = useState(false)
 
   //  ------ Wagmi -------
 
@@ -110,6 +139,7 @@ const ReqToRegister = ({
     ...configCommit,
     onSuccess(data) {
       console.log('Success commit', data)
+      setIsCommitting(true)
     },
     onError(error) {
       console.log('Error commit', error)
@@ -123,6 +153,7 @@ const ReqToRegister = ({
         if (receipt.status == 1) {
           console.log('Commit transaction succeeded!', receipt.logs)
           setCount(count + 1)
+          setIsCommitting(false)
           wait()
           return
         }
@@ -236,6 +267,8 @@ const ReqToRegister = ({
           </button>
         ) : isWaiting ? (
           <Waiting />
+        ) : isCommitting ? (
+          <Committing />
         ) : (
           <>
             <button
