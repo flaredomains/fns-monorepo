d67a16ff148c8bc9ca8c32a065ef18175200746b
Chase Brown <Chasebrown7@icloud.com>
2023-05-10 10:47:34 -0700

    new icons

--- apps/next/src/pages/my_account/index.tsx
+++ apps/next/src/pages/my_account/index.tsx
@@ -5,13 +5,13 @@ import MyAccount from '../../../components/MyAccount'
 export default function My_Account() {
   return (
     <>
-      <div className="min-h-full">
+      <div className="min-h-screen lg:min-h-full">
         <div className="flex-col bg-[#0F172A] lg:flex lg:flex-row">
           {/* Left Side / Navbar */}
           <Side_Navbar />
 
           {/* Register */}
-          <div className="flex-col mt-9 pb-8 lg:mx-8 w-full min-h-full lg:w-3/4">
+          <div className="flex-col mt-9 pb-8 lg:mx-8 w-full min-h-screen lg:min-h-full lg:w-3/4">
             <MyAccount />
           </div>
         </div>
