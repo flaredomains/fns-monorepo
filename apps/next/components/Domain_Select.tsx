54702b3233c4799dc798e3dc0d4dfe9091fa670b
chefleo <simon.digiorgio@gmail.com>
2023-05-16 09:21:28 +0200

    Revert "Remove search icon after name search"
    This reverts commit 2089c26df215185e82d62536f9dec5aa1a7c299a.

--- apps/next/components/Domain_Select.tsx
+++ apps/next/components/Domain_Select.tsx
@@ -11,6 +11,22 @@ function Domain_Select({ result }: { result: string }) {
             ? `${result.slice(0, 6)}...${result.slice(-4)}`
             : result}
         </p>
+        {/* Search Icon */}
+        <div className="h-7 w-7 mr-1">
+          <svg
+            viewBox="0 0 19 19"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M17.5572 17.3121L12.3181 12.0729M14.0644 7.70702C14.0644 11.0828 11.3279 13.8193 7.95214 13.8193C4.57641 13.8193 1.83984 11.0828 1.83984 7.70702C1.83984 4.33129 4.57641 1.59473 7.95214 1.59473C11.3279 1.59473 14.0644 4.33129 14.0644 7.70702Z"
+              stroke="#94A3B8"
+              strokeWidth="1.87485"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
       </div>
     </>
   )
