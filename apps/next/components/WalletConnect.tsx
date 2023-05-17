7364ce1ea0f53d4bcae5654f777b27ede1e5c863
MatteoDigiorgio <matthew.digiorgio@gmail.com>
2023-05-17 15:21:45 +0200

    Resolved the problem where the 'Connect Wallet' section was completely absent in the mobile view.

--- apps/next/components/WalletConnect.tsx
+++ apps/next/components/WalletConnect.tsx
@@ -23,29 +23,29 @@ const WalletConnectSection = () => {
   return (
     <>
       {/* Badge -- Icon wallet connected */}
-      <div className="mt-20 flex justify-center items-center bg-[#334155] h-6 text-white px-auto rounded-full mx-auto">
+      <div className="flex justify-center items-center bg-[#334155] h-6 text-white px-2 mx-2 rounded-full shrink-0">
         <Image
           className="h-4 w-4 mr-2"
           src={isConnected ? Network : X}
           alt="FNS"
         />
-        <p className="text-xs font-medium">
+        <p className="text-xs font-medium shrink-0">
           {isConnected ? `${chain?.name} Network` : 'Wallet Not Connected'}
         </p>
       </div>
       {/* Messages */}
-      <p className="text-white font-bold text-xl mt-2 text-center">
+      <p className="text-white font-bold text-xl mt-2 mx-2 text-center">
         {isConnected
           ? 'Your wallet is connected'
           : 'Your wallet is not connected'}
       </p>
       {!isConnected && (
-        <p className="text-[#94A3B8] text-[0.625rem] mt-2 text-center">
+        <p className="text-[#94A3B8] text-[0.625rem] mt-2 mx-2 text-center">
           Please connect your wallet to register your FNS domain.
         </p>
       )}
       {/* WebConnect Button */}
-      <div className="mt-4 mb-12 flex justify-center items-center text-center h-8 rounded-lg text-white px-auto mx-auto">
+      <div className="mt-2 flex justify-center items-center text-center rounded-lg text-white px-auto mx-auto">
         <Web3Button icon="hide" label="Connect Wallet +" balance="hide" />
       </div>
     </>
@@ -56,10 +56,8 @@ function WalletConnect() {
   return (
     <>
       {/* Wallet connect */}
-      <div className="hidden lg:flex lg:w-1/4">
-        <div className="flex-col justify-center items-center w-full h-80 bg-gray-800 rounded-lg px-1 xl:px-7">
-          <WalletConnectSection />
-        </div>
+      <div className="flex flex-col w-full lg:w-1/4 h-52 lg:h-80 justify-center items-center mt-9 lg:mt-0 px-1 xl:px-7 bg-gray-800 rounded-lg shrink-0">
+        <WalletConnectSection />
       </div>
     </>
   )
