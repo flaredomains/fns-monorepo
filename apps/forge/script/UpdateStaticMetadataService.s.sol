68773ae2cacc751c84761491996b29be89def8ef
RickyOnTheBlock <rickyontheblock@pm.me>
2023-07-04 18:42:34 -0400

    Stripped Down UpdateStaticMetadataService Script

---
+++ apps/forge/script/UpdateStaticMetadataService.s.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.8.10;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+
+import "fns/registry/FNSRegistry.sol";
+import "fns/resolvers/PublicResolver.sol";
+import "fns/flr-registrar/BaseRegistrar.sol";
+import "fns/chain-state/MintedDomainNames.sol";
+import "fns/registry/ReverseRegistrar.sol";
+import "fns/wrapper/NameWrapper.sol";
+import "fns/wrapper/StaticMetadataService.sol";
+import "fns/flr-registrar/FLRRegistrarController.sol";
+import "fns/flr-registrar/mock/MockStablePriceOracle.sol";
+import "fns/flr-registrar/DummyOracle.sol";
+import "fns/no-collisions/NoNameCollisions.sol";
+
+import "fns-test/utils/FNSNamehash.sol";
+
+contract Template is Script {
+    // Anvil Wallets
+    address immutable ANVIL_DEPLOYER_ADDRESS = vm.envAddress("ANVIL_DEPLOYER_ADDRESS");
+    uint256 immutable ANVIL_DEPLOYER_PRIVATE_KEY = vm.envUint("ANVIL_DEPLOYER_PRIVATE_KEY");
+    address immutable ANVIL_OWNER_ADDRESS = vm.envAddress("ANVIL_OWNER_ADDRESS");
+    uint256 immutable ANVIL_OWNER_PRIVATE_KEY = vm.envUint("ANVIL_OWNER_PRIVATE_KEY");
+
+    // Testnet Wallets
+    address immutable DEPLOYER_ADDRESS = vm.envAddress("DEPLOYER_ADDRESS");
+    uint256 immutable DEPLOYER_PRIVATE_KEY = vm.envUint("DEPLOYER_PRIVATE_KEY");
+    address immutable OWNER_ADDRESS = vm.envAddress("OWNER_ADDRESS");
+    uint256 immutable OWNER_PRIVATE_KEY = vm.envUint("OWNER_PRIVATE_KEY");
+
+    bytes32 constant rootNode = 0x0;
+
+    constructor() {
+        console.log("constructor()");
+    }
+
+    function setUp() public {
+        console.log("setUp()");
+    }
+
+    // Entrypoint to deploy script
+    function run() external {
+        vm.startBroadcast(DEPLOYER_PRIVATE_KEY);
+
+        // Begin script specifics
+        console.log("run()");
+        StaticMetadataService staticMetadataService = new StaticMetadataService("https://fns.infura-ipfs.io/ipfs/QmSmtZszSZPfHokzKzcvaWxBbrnduiVCho6fDkaVjFPM4x");
+        NameWrapper nameWrapper = NameWrapper(0xd41653631E077f2A5B2fcDE6fE30501753222D90);
+        nameWrapper.setMetadataService(staticMetadataService);
+
+        vm.stopBroadcast();
+    }
+}
