d6854f69655129abca8d21d8a3a2b9a9981e64d9
chefleo <simon.digiorgio@gmail.com>
2023-08-13 16:25:52 +0200

    Added dynamic page website

---
+++ apps/next/src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Search from "../../components/Search";
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen">
+      <Search />
+    </div>
+  );
+}
---
+++ apps/next/src/pages/[website].tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import PageWebsite from "../../components/Websites/PageWebsite";
+
+export default function Website() {
+  const router = useRouter();
+  // const { domain } = router.query;
+
+  useEffect(() => {
+    if (!router.isReady) return;
+
+    const domain = router.query.website as string;
+    console.log(domain);
+    if (typeof domain === "string" && !domain.endsWith(".flr")) {
+      // Redirect to 404 page if URL doesn't end with ".flr"
+      router.push("/404");
+    }
+  }, [router.isReady, router.query]);
+
+  return (
+    <div className="min-h-screen">
+      <PageWebsite />
+    </div>
+  );
+}
