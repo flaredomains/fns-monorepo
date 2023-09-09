e102007210bb5e2f590e70871b1a3537f0e747b0
MatteoDigiorgio <matthew.digiorgio@gmail.com>
2023-09-09 18:50:06 +0200

    Send tokens implemented + Balance

--- apps/next/components/SendTokens/index.tsx
+++ apps/next/components/SendTokens/index.tsx
@@ -1,6 +1,4 @@
-import { Domain } from 'domain';
 import React, { useState } from 'react';
-import { useAccount, useContractRead } from 'wagmi';
 import SendOrange from '../../public/Send_orange.png';
 import SendWhite from '../../public/Send_white.png';
 import Image from 'next/image';
@@ -8,15 +6,71 @@ import styles from '../../src/styles/Main.module.css';
 
 import WalletConnect from '../WalletConnect';
 import FlareLogo from '../../public/FlareLogo.png';
-
+import { useDebounce } from 'use-debounce';
+import {
+  usePrepareSendTransaction,
+  useSendTransaction,
+  useWaitForTransaction,
+  useAccount,
+  useBalance,
+  useContractRead,
+} from 'wagmi';
 import { BigNumber, utils } from 'ethers';
 import NameWrapper from '../../src/pages/abi/NameWrapper.json';
 
 const ZERO_ADDRESS: string = '0x0000000000000000000000000000000000000000';
 
+const GetBalance = () => {
+  const { address, isConnecting, isDisconnected } = useAccount();
+  const { data, isError, isLoading } = useBalance({
+    address: address,
+  });
+  const truncatedFormatted = data?.formatted
+    ? data.formatted.split('.')[0] +
+      '.' +
+      data.formatted.split('.')[1].slice(0, 4)
+    : '';
+
+  if (isLoading)
+    return (
+      <div className='flex w-full justify-center mb-6 text-slate-400'>
+        Fetching balance…
+      </div>
+    );
+  if (isError)
+    return (
+      <div className='flex w-full justify-center mb-6 text-slate-400'>
+        Error fetching balance
+      </div>
+    );
+  return (
+    <div className='flex w-full justify-center mb-6 text-slate-400'>
+      Your Balance: {truncatedFormatted}... {data?.symbol}
+    </div>
+  );
+};
+
 function SendTokens() {
   const [isOpen, setIsOpen] = useState(false);
-  const [addressDomain, setAddressDomain] = useState<Array<Domain>>([]);
+  const [to, setTo] = React.useState('');
+  const [finalTo, setFinalTo] = React.useState('0');
+  const [debouncedTo] = useDebounce(to, 500);
+
+  const [amount, setAmount] = React.useState('0');
+  const [finalAmount, setFinalAmount] = React.useState('0');
+  const [debouncedAmount] = useDebounce(amount, 500);
+
+  const { config } = usePrepareSendTransaction({
+    request: {
+      to: debouncedTo,
+      value: debouncedAmount ? utils.parseEther(debouncedAmount) : undefined,
+    },
+  });
+  const { data, sendTransaction } = useSendTransaction(config);
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
 
   const { address, isConnected } = useAccount();
   const [hash, setHash] = useState<string>('');
@@ -33,13 +87,16 @@ function SendTokens() {
     args: [hash],
     onSuccess(data: string) {
       setIsValid(data !== ZERO_ADDRESS);
+      if (data !== ZERO_ADDRESS) {
+        setTo(data);
+      }
     },
     onError(error) {
       console.log('Error ownerOf', error);
     },
   });
 
-  const handleInput = (e: {
+  const handleReceiverInput = (e: {
     target: { value: React.SetStateAction<string | undefined> };
   }) => {
     const inputValue = e.target.value as string;
@@ -52,6 +109,12 @@ function SendTokens() {
     }
 
     setInputUsable(isFlrInput);
+  };
+
+  const handleAmountInput = (e: {
+    target: { value: React.SetStateAction<string | undefined> };
+  }) => {
+    setAmount(e.target.value as string);
   };
   return (
     <div
@@ -84,7 +147,15 @@ function SendTokens() {
 
         {isConnected ? (
           <div className='flex flex-col items-center py-4 mb-4 mt-10'>
-            <form className='flex flex-col w-full justify-center'>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                setFinalTo(to);
+                setFinalAmount(amount);
+                sendTransaction?.();
+              }}
+              className='flex flex-col w-full justify-center'
+            >
               <div
                 className={`flex items-center mx-auto w-3/4 py-2 px-4 mb-4 h-12 rounded-md bg-gray-700 border-2 border-gray-500 ${styles.autofill}`}
               >
@@ -96,7 +167,7 @@ function SendTokens() {
                 <input
                   type='text'
                   name='receiver-name'
-                  onChange={handleInput}
+                  onChange={handleReceiverInput}
                   onInput={(event) => {
                     const inputElement = event.target as HTMLInputElement;
                     inputElement.value === ''
@@ -145,14 +216,12 @@ function SendTokens() {
                 )}
               </div>
               <div
-                className={`flex items-center mx-auto w-1/2 py-2 px-4 h-12 mb-4 rounded-md bg-gray-700 border-2 border-gray-500 ${styles.autofill}`}
+                className={`flex items-center mx-auto w-1/2 py-2 px-4 h-12 mb-2 rounded-md bg-gray-700 border-2 border-gray-500 ${styles.autofill}`}
               >
                 <input
                   type='text'
                   name='token-value'
-                  onChange={(e) => {
-                    console.log(e.target.value.toLowerCase());
-                  }}
+                  onChange={handleAmountInput}
                   onInput={(event) => {
                     const inputElement = event.target as HTMLInputElement;
                     inputElement.setCustomValidity('');
@@ -163,14 +232,35 @@ function SendTokens() {
                   required
                 />
               </div>
+              <GetBalance />
               <button
-                disabled={!isValid}
+                disabled={
+                  (!isValid && parseFloat(amount) === 0) ||
+                  amount === '' ||
+                  isLoading ||
+                  !sendTransaction
+                }
                 className='flex w-48 mx-auto justify-center items-center px-6 py-3 bg-[#F97316] h-12 rounded-lg text-white px-auto hover:scale-105 transform transition duration-300 ease-out disabled:border-gray-500 disabled:bg-gray-500 disabled:hover:scale-100 disabled:cursor-not-allowed'
               >
-                <p className='text-base font-semibold mr-1'>Send Tokens</p>
+                <p className='text-base font-semibold mr-1'>
+                  {isLoading ? 'Sending...' : 'Send Tokens'}
+                </p>
                 <Image className='h-6 w-6' src={SendWhite} alt='Send' />
               </button>
             </form>
+            {isSuccess && (
+              <div className='flex flex-col w-full justify-center items-center mt-8 text-white'>
+                Successfully sent {finalAmount} Flare to {finalTo}
+                <div>
+                  <a
+                    href={`https://etherscan.io/tx/${data?.hash}`}
+                    className='flex mt-2 py-1 px-4 rounded-full bg-[#F97316] hover:scale-105 transform transition duration-300 ease-out'
+                  >
+                    Etherscan
+                  </a>
+                </div>
+              </div>
+            )}
           </div>
         ) : (
           <div className='flex-col py-4 mb-4 mt-10'>
