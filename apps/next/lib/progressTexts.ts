81432697269c452a75c291fe55097745a5402b37
Ric Waltman <rickyontheblock@pm.me>
2023-09-16 16:37:15 -0700

    Fix steps text

--- apps/next/lib/progressTexts.ts
+++ apps/next/lib/progressTexts.ts
@@ -2,12 +2,12 @@ const progressArr = [
   {
     number: 1,
     stepText: "Select Domain",
-    descriptionText: `Select your domain for website minting below. Once a website is minted, it will be displayed at the selected domain. Website will be viewable on Brave and Opera browsers. More browsers to be added.`,
+    descriptionText: `Select your domain for website minting. Once a website is minted, it will be displayed at the selected domain. Website will be viewable on all browsers.`,
   },
   {
     number: 2,
     stepText: "Upload Assets",
-    descriptionText: `Framework Functionality: Background image must be900x1200 pixels and no bigger than 5MB, title and body text are center aligned, website supports one font at this time, contact button link must include: 'mailto' See FAQ.`,
+    descriptionText: `Framework Functionality: Background image must be 900x1200 pixels and no bigger than 5MB, title and body text are center aligned, website supports one font at this time, contact button link must include: 'mailto' See FAQ.`,
   },
   {
     number: 3,
